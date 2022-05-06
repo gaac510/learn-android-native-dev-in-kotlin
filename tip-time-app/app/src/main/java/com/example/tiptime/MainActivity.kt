d264124d9f3f20c4eba304058a760c4c1b945e38
gaac510 <fr0zen510@gmail.com>
2022-05-06 22:03:15 +1000

    Write instrumentation tests

--- tip-time-app/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ tip-time-app/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -17,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.calculateTipButton.setOnClickListener {
             calculateTip()
+        }
+        binding.costOfService.setOnKeyListener {
+            view, keyCode, _ -> handleKeyEvent(view, keyCode)
         }
     }
 
@@ -38,5 +45,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.tipAmount.text = getString(R.string.tip_amount, NumberFormat.getCurrencyInstance().format(tipAmount))
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if(keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
